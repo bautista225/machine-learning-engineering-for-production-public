d7b917695aa5a72413f7bef1955497b0c70b49b0
Juan Bautista <juangarciatraver@gmail.com>
2024-09-24 13:21:37 +0200

    Adding new classifier with scaling

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -3,8 +3,20 @@ import numpy as np
 from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel, conlist
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 
-# Comment
+def test_pipeline_and_scaler():
+
+    # Check if clf is an instance of sklearn.pipeline.Pipeline
+    isPipeline = isinstance(clf, Pipeline)
+    assert isPipeline
+
+    if isPipeline:
+        # Check if first step of pipeline is an instance of
+        # sklearn.preprocessing.StandardScaler
+        firstStep = [v for v in clf.named_steps.values()][0]
+        assert isinstance(firstStep, StandardScaler)
 
 app = FastAPI(title="Predicting Wine Class with batching")
 
